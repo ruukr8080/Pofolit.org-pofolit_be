25fb97d854ba6d05af2c3f537751da9d81ac4e63
han <ruukr8080@gmail.com>
2025-08-08 17:44:30 +0900

    init

---
+++ src/main/java/com/pofolit_be/PofolitBeApplication.java
@@ -0,0 +1,13 @@
+package com.pofolit_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PofolitBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PofolitBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pofolit_be/PofolitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pofolit_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PofolitBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
