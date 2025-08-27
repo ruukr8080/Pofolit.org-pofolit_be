6de419a658fec80213a421a93282fdbb79c20f79
ruukr8080 <ruukr8080@gmail.com>
2025-08-28 08:09:17 +0900

    변경

--- src/main/java/com/app/pofolit_be/PofolitBeApplication.java
+++ src/main/java/com/app/pofolit_be/PofolitBeApplication.java
@@ -2,12 +2,24 @@ package com.app.pofolit_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PofolitBeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PofolitBeApplication.class, args);
-    }
+   public static void main(String[] args) {
+      SpringApplication.run(PofolitBeApplication.class, args);
+   }
 
+}
+
+@RequestMapping("/health")
+@RestController
+class WebHealthChekController {
+   @GetMapping
+   public String healthCheck() {
+      return "good";
+   }
 }
