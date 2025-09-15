20e818cd0fd5e7ab8a12994ff7b6b10b42d6a227
ruukr8080 <ruukr8080@gmail.com>
2025-09-16 07:51:09 +0900

    feat : 암호화 방식 설정,
    - 비대칭키 : 공개키,비밀키.
    - 알고리즘 : jjwt가 지원해주는거 RSA
    - 현재 bit : '1024'.
    
    현재 권장 bit는 2028.
    bit는 2030년 기준 3072 이상으로 확대 권장.

--- src/main/java/com/app/pofolit_be/security/config/JwtProperties.java
+++ src/main/java/com/app/pofolit_be/security/config/JwtProperties.java
@@ -1,12 +1,20 @@
 package com.app.pofolit_be.security.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+import java.time.Instant;
 
 /**
  * jjwt 0.12.6
  * JwtHeaderProperties record
  * JwtHeader
- * JwtPayloadProperties  record
+ * JwtPayloadProperties record
  * JwtPayloadMapper
  * JwtDigest
  * signature
@@ -21,18 +29,36 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * 여기에도 NumericDate 형식으로 날짜를 지정하며, 이 날짜가 지나기 전까지는 토큰이 처리되지 않습니다.
  * iat: 토큰이 발급된 시간 (issued at), 이 값을 사용하여 토큰의 age 가 얼마나 되었는지 판단 할 수 있습니다.
  * jti: JWT의 고유 식별자로서, 주로 중복적인 처리를 방지하기 위하여 사용됩니다. 일회용 토큰에 사용하면 유용합니다.
- *
- * @param secret "sign with claim" ${SECRET_KEY} ("arg":"HS512")
- * @param aud Role{ guest,user }
- * @param iss Pofolit
- * @param ttl Time to live (180초 / 1일)
+ * <p>
+ * param algorithm "sign with algorithm" ${SECRET_KEY} ("arg":"HS512")
+ * param `keySize` keySize
+ * param `iss` "pofolit"
  */
+@Slf4j
 @ConfigurationProperties(prefix = "spring.jwt")
 public record JwtProperties(
-        String secret,
+        int keySize,
         String iss,
-        String aud,
-        long ttl
-){
+        long pTtl,
+        long aTtl,
+        long rTtl
+)
+{
+    @Bean
+    public KeyPair rsaKeyPair() {
+        Instant start = Instant.now();
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(keySize);
+            KeyPair keyPair = keyGen.generateKeyPair();
+            Instant end = Instant.now();
+            log.info("RSA KeyPair | 암호키 bit : {} \n생성하는데 걸린시간: {}ms"
+                    , keySize, Duration.between(start, end).toMillis());
+            return keyPair;
 
+        } catch (NoSuchAlgorithmException e) {
+            log.error("RSA KeyPair \n 암호키 bit: [{}] ", keySize);
+            throw new IllegalStateException("암호키 알고리즘 변경해야 합니다.", e);
+        }
+    }
 }
